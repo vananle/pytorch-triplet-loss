8785795446d7d4ed1aa46d156ef13b603e5fd85e
vananle <vananle1993@gmail.com>
2021-12-18 15:09:15 +0900

    add accuracy

---
+++ CIFAR10_online_mining_all_Simple_Model.py
@@ -0,0 +1,110 @@
+# %%
+import torch
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10, MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.manifold import TSNE
+
+from TNN import Mining, Model
+from TNN.Plot import scatter
+from TNN.Loss_Fn import triplet_loss
+
+# %%
+device = torch.device('cuda:1')
+tsne = TSNE(random_state=0)
+batch_size_train = 256
+
+# %%
+train_loader = DataLoader(CIFAR10('./CIFAR10/', train=True, download=True,
+                                transform=Compose([ToTensor(),Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                               ),batch_size=batch_size_train, shuffle=True)
+test_loader = DataLoader(CIFAR10('./CIFAR10/', train=False, download=True,
+                                transform=Compose([ToTensor(),Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                               ),batch_size=batch_size_train, shuffle=False)
+
+# %%
+x_test, y_test = next(iter(test_loader))
+x_test = x_test.to(device)
+y_test = y_test.to(device)
+
+# %%
+x_train, y_train = next(iter(train_loader))
+x_train = x_train.to(device)
+y_train = y_train.to(device)
+
+# %%
+train_tsne_embeds = tsne.fit_transform(x_train.flatten(1).cpu().detach().numpy())
+test_tsne_embeds = tsne.fit_transform(x_test.flatten(1).cpu().detach().numpy())
+
+# %%
+scatter(train_tsne_embeds, y_train.cpu().numpy(), subtitle=f'online Original MNIST distribution (train set)', dataset='CIFAR10')
+scatter(test_tsne_embeds, y_test.cpu().numpy(), subtitle=f'online Original MNIST distribution (test set)', dataset='CIFAR10')
+
+# %%
+model = Model.SimpleTNN(input_shape=x_train.shape[1:], output_size=128)
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+# %%
+margin = 0.5
+for epoch in range(300):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    model.train()
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss, pos_triplet, valid_triplet = Mining.online_mine_all(labels, outputs, margin=margin, squared=True, device=device)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i%2 == 0:
+            print(f"At epoches = {epoch}, i = {i}, loss = {loss:.5f}, trainable_triplet = {pos_triplet}, total_triplets = {valid_triplet}"
+                  , end='\r')
+    train_epoch_loss = running_loss / len(train_loader)
+
+    # Evaluating on test set
+    print(" "*100)
+    running_loss = 0.0
+    model.eval()
+    for i, data in enumerate(test_loader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        outputs = model(inputs)
+        loss, pos_triplet, valid_triplet = Mining.online_mine_all(labels, outputs, margin=margin, squared=True, device=device)
+        running_loss += loss.item()
+        if i%2 == 0:
+            print(f"Evaluating At epoches = {epoch}, i = {i}, loss = {loss:.5f}", end='\r')
+    test_epoch_loss = running_loss / len(test_loader)
+    
+    print(" "*100)
+    print(f"At epoches = {epoch}, epoch_train_loss = {train_epoch_loss:.6f},\tepoch_test_loss = {test_epoch_loss:.6f}")
+print('Finished Training')
+
+# %%
+train_outputs = model(x_train)
+test_outputs = model(x_test)
+train_tsne_embeds = tsne.fit_transform(train_outputs.cpu().detach().numpy())
+test_tsne_embeds = tsne.fit_transform(test_outputs.cpu().detach().numpy())
+
+scatter(train_tsne_embeds, y_train.cpu().numpy(), subtitle=f'online TNN distribution (train set)', dataset='CIFAR10')
+scatter(test_tsne_embeds, y_test.cpu().numpy(), subtitle=f'online TNN distribution (test set)', dataset='CIFAR10')
+
+# %%
+
+
+# %%
---
+++ CIFAR10_online_mining_all_overfitting.py
@@ -0,0 +1,89 @@
+# %%
+import torch
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10, MNIST
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.manifold import TSNE
+
+from TNN import Mining, Model
+from TNN.Plot import scatter
+from TNN.Loss_Fn import triplet_loss
+
+# %%
+device = torch.device('cuda:1')
+tsne = TSNE(random_state=0)
+batch_size_train = 256
+
+# %%
+train_loader = DataLoader(CIFAR10('./CIFAR10/', train=True, download=True,
+                                transform=Compose([ToTensor(),Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                               ),batch_size=batch_size_train, shuffle=True)
+test_loader = DataLoader(CIFAR10('./CIFAR10/', train=False, download=True,
+                                transform=Compose([ToTensor(),Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                               ),batch_size=batch_size_train, shuffle=False)
+
+# %%
+x_test, y_test = next(iter(test_loader))
+x_test = x_test.to(device)
+y_test = y_test.to(device)
+
+# %%
+x_train, y_train = next(iter(train_loader))
+x_train = x_train.to(device)
+y_train = y_train.to(device)
+
+# %%
+train_tsne_embeds = tsne.fit_transform(x_train.flatten(1).cpu().detach().numpy())
+test_tsne_embeds = tsne.fit_transform(x_test.flatten(1).cpu().detach().numpy())
+
+# %%
+scatter(train_tsne_embeds, y_train.cpu().numpy(), subtitle=f'online Original MNIST distribution (train set)', dataset='CIFAR10')
+scatter(test_tsne_embeds, y_test.cpu().numpy(), subtitle=f'online Original MNIST distribution (test set)', dataset='CIFAR10')
+
+# %%
+model = Model.TNN_CIFAR10(input_shape=x_train.shape[1:], output_size=32)
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+# %%
+margin = 1e-2
+for epoch in range(300):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss, pos_triplet, valid_triplet = Mining.online_mine_all(labels, outputs, margin=margin, squared=True, device=device)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i%2 == 0:
+            print(f"At epoches = {epoch}, i = {i}, loss = {loss:.5f}, trainable_triplet = {pos_triplet}, total_triplets = {valid_triplet}"
+                  , end='\r')
+    epoch_loss = running_loss / len(train_loader)
+    
+    print(" "*100)
+    print(f"At epoches = {epoch}, epoch_loss = {epoch_loss}")
+print('Finished Training')
+
+# %%
+train_outputs = model(x_train)
+test_outputs = model(x_test)
+train_tsne_embeds = tsne.fit_transform(train_outputs.cpu().detach().numpy())
+test_tsne_embeds = tsne.fit_transform(test_outputs.cpu().detach().numpy())
+
+scatter(train_tsne_embeds, y_train.cpu().numpy(), subtitle=f'online TNN distribution (train set)', dataset='CIFAR10')
+scatter(test_tsne_embeds, y_test.cpu().numpy(), subtitle=f'online TNN distribution (test set)', dataset='CIFAR10')
+
+# %%
